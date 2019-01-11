1ac1ceed3a46548fed7d058a43a1a850bd564bdc
James Almond <jamestalmond@gmail.com>
2019-01-11 15:33:08 +0000

    Mapped other actions to counter

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,10 +3,32 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-	if (action.type === 'INCREMENT') {
-		return {
-			counter: state.counter + 1
-		};
+	switch (action.type) {
+		case 'INCREMENT':
+			return {
+				counter: state.counter + 1
+			};
+			break;
+		case 'DECREMENT':
+			return {
+				counter: state.counter - 1
+			};
+			break;
+		case 'ADD':
+			return {
+				counter: state.counter + 5
+			};
+			break;
+		case 'SUBTRACT':
+			return {
+				counter: state.counter - 5
+			};
+			break;
+		default:
+			return {
+				counter: state.counter
+			};
+			break;
 	}
 
 	return state;
